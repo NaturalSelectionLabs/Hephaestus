5b50b51caaf49a989ab31d82ccc41fcf6c75954f
sljeff <shanliangjeff@qq.com>
2022-03-16 16:23:57 +0800

    traefik prod

--- apply.sh
+++ apply.sh
@@ -1,8 +1,8 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-VALUES=traefik-mesh/prod/values.yaml
+RELEASE=traefik
+CHART=traefik/traefik
+VALUES=traefik/prod/values.yaml
 NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
