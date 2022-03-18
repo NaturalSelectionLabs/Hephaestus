fb96484ae6af96bc3d617d6fb5f91c6e3d629144
sljeff <shanliangjeff@qq.com>
2022-03-18 14:52:25 +0800

    install prom

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
-CLUSTER_NAME=development
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-VALUES=traefik-mesh/dev/values.yaml
-NS=default
+CLUSTER_NAME=production
+RELEASE=prometheus
+CHART=prometheus-community/prometheus
+VALUES=prometheus/prod/values.yaml
+NS=guardian
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
