f527d0b05e73036c76fb52e7f4380cedba0a639c
sljeff <shanliangjeff@qq.com>
2022-03-23 16:38:15 +0800

    update grafana rooturl

--- apply.sh
+++ apply.sh
@@ -1,8 +1,8 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-VALUES=traefik-mesh/prod/values.yaml
+RELEASE=grafana
+CHART=grafana/grafana
+VALUES=grafana/prod/values.yaml
 NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
