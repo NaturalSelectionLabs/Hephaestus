6db968dbbea5db683b99c7f3eedc5ae971aba48d
sljeff <shanliangjeff@qq.com>
2022-03-23 16:40:35 +0800

    fix grafana namespace

--- apply.sh
+++ apply.sh
@@ -3,7 +3,7 @@ CLUSTER_NAME=production
 RELEASE=grafana
 CHART=grafana/grafana
 VALUES=grafana/prod/values.yaml
-NS=default
+NS=guardian
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
