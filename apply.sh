c4524dfc34b89e285f59d4460292da9616c3a7bb
sljeff <shanliangjeff@qq.com>
2022-04-02 15:24:35 +0800

    feat: loki and promtail

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-VALUES=traefik-mesh/prod/values.yaml
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-NS=default
+VALUES=loki/prod/values.yaml
+RELEASE=loki
+CHART=grafana/loki-stack
+NS=guardian
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
