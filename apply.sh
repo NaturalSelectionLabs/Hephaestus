823dc336b3a5df611a7b429c23a0fd8e5ea16f52
sljeff <shanliangjeff@qq.com>
2022-04-02 15:12:19 +0800

    feat: traefik-mesh log format json dev

--- apply.sh
+++ apply.sh
@@ -1,8 +1,8 @@
 AWS_REGION=us-west-2
-CLUSTER_NAME=production
-VALUES=traefik/prod/values.yaml
-RELEASE=traefik
-CHART=traefik/traefik
+CLUSTER_NAME=development
+VALUES=traefik-mesh/dev/values.yaml
+RELEASE=traefik-mesh
+CHART=traefik-mesh/traefik-mesh
 NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
