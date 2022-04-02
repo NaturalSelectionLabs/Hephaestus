1d34092e5df730ef23ca9725a77d049f71e6e724
sljeff <shanliangjeff@qq.com>
2022-04-02 15:05:31 +0800

    feat: traefik log format json dev

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
-CLUSTER_NAME=production
-RELEASE=grafana
-CHART=grafana/grafana
-VALUES=grafana/prod/values.yaml
-NS=guardian
+CLUSTER_NAME=development
+RELEASE=traefik
+CHART=traefik/traefik
+VALUES=traefik/dev/values.yaml
+NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
