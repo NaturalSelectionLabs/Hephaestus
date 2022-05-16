b4accf88763d9cbdf6cf9269a3732602e11dec77
sljeff <shanliangjeff@qq.com>
2022-05-16 15:57:46 +0800

    helm jaeger

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-VALUES=traefik-mesh/prod/values.yaml
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-NS=default
+VALUES=jaeger/prod/values.yaml
+RELEASE=jaeger
+CHART=jaegertracing/jaeger
+NS=guardian
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
