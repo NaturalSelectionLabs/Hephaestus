9b2be7e811055acff1222907dec6ba94e61da87f
sljeff <shanliangjeff@qq.com>
2022-05-16 17:05:57 +0800

    helm jaeger: add to traefik

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-VALUES=jaeger/prod/values.yaml
-RELEASE=jaeger
-CHART=jaegertracing/jaeger
-NS=guardian
+VALUES=traefik/prod/values.yaml
+RELEASE=traefik
+CHART=traefik/traefik
+NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
