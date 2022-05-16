3678fa775a6a150e1a1ddec03a3e7ffdccdc0311
sljeff <shanliangjeff@qq.com>
2022-05-16 11:36:05 +0800

    traefik jaeger prod: API

--- apply.sh
+++ apply.sh
@@ -1,8 +1,8 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-VALUES=traefik-mesh/prod/values.yaml
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
+VALUES=traefik/prod/values.yaml
+RELEASE=traefik
+CHART=traefik/traefik
 NS=default
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
