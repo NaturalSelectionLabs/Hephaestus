3446ab06462523d821cbdefa93cdce5b5e357b61
sljeff <shanliangjeff@qq.com>
2022-05-20 16:42:00 +0800

    feat: rabbit exporter

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 AWS_REGION=us-west-2
 CLUSTER_NAME=production
-VALUES=traefik-mesh/prod/values.yaml
-RELEASE=traefik-mesh
-CHART=traefik-mesh/traefik-mesh
-NS=default
+VALUES=exporters/rabbitmq/values.yaml
+RELEASE=rabbitmq-exporter
+CHART=prometheus-community/prometheus-rabbitmq-exporter
+NS=guardian
 
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
