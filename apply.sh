915b79835b364828ad043be95525c8834f9dfa7b
sljeff <shanliangjeff@qq.com>
2022-07-18 13:56:53 +0800

    feat: traefik mesh dev

--- apply.sh
+++
@@ -1,8 +0,0 @@
-AWS_REGION=us-west-2
-CLUSTER_NAME=production
-VALUES=traefik/dev/values.yaml
-RELEASE=traefik
-CHART=traefik/traefik
-NS=default
-
-helm upgrade $RELEASE $CHART -f $VALUES -i -n $NS
