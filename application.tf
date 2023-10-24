fe5bf5429698fc91d702d7be513442047b1e0415
incubator4 <aries0robin@gmail.com>
2023-10-24 18:17:47 +0800

    feat: add cert-manager

--- application.tf
+++ application.tf
@@ -142,7 +142,6 @@ resource "argocd_application_set" "cert_manager" {
           repo_url = var.repo_url
           target_revision = "HEAD"
           path = "cert-manager/{{cluster}}"
-          kustomize {}
         }
 
         destination {
