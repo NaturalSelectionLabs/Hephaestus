8dab8b02176117baf7edd4e1d1a05553f9f7cef2
incubator4 <aries0robin@gmail.com>
2023-10-26 15:36:10 +0800

    feat: add jaeger

--- application.tf
+++ application.tf
@@ -79,3 +79,44 @@ resource "argocd_application" "keycloak" {
     }
   }
 }
+
+resource "argocd_application" "jaeger" {
+  metadata {
+    name      = "jaeger"
+    namespace = "guardian"
+  }
+  spec {
+    project = argocd_project.guardian.metadata[0].name
+    source {
+      helm {
+        release_name = "jaeger"
+        value_files = [
+          "$values/jaeger/prod/values.yaml"
+        ]
+      }
+      repo_url        = "https://jaegertracing.github.io/helm-charts"
+      target_revision = "0.71.x"
+      chart           = "jaeger"
+    }
+
+    source {
+      repo_url        = var.repo_url
+      target_revision = "HEAD"
+      ref             = "values"
+    }
+
+    source {
+      repo_url        = var.repo_url
+      target_revision = "HEAD"
+      path            = "jaeger/prod"
+      plugin {
+        name = "avp-kustomize"
+      }
+    }
+
+    destination {
+      server    = argocd_cluster.prod.server
+      namespace = "guardian"
+    }
+  }
+}
