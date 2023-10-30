0773062db7cf2194d56b15040d754ffc245c9397
incubator4 <aries0robin@gmail.com>
2023-10-30 17:12:29 +0800

    add more project

--- project.tf
+++ project.tf
@@ -12,17 +12,47 @@ resource "argocd_project" "guardian" {
       group = "*"
       kind  = "*"
     }
-
     destination {
       server    = argocd_cluster.prod.server
       namespace = "*"
     }
-
     destination {
       server    = argocd_cluster.dev.server
       namespace = "*"
     }
+  }
+}
 
+resource "argocd_project" "namespaced" {
+  for_each = toset([
+    "ai",
+    "copilot",
+    "crossbell",
+    "network",
+    "pregod",
+    "tureco",
+  ])
+
+  metadata {
+    name      = each.value
+    namespace = each.value
   }
 
+  spec {
+    source_namespaces = [each.value]
+    source_repos      = ["*"]
+
+    cluster_resource_whitelist {
+      group = "*"
+      kind  = "*"
+    }
+    destination {
+      server    = argocd_cluster.prod.server
+      namespace = "*"
+    }
+    destination {
+      server    = argocd_cluster.dev.server
+      namespace = "*"
+    }
+  }
 }
