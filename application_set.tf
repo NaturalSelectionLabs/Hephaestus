688a0bf03c850d408ba0a54ac9eafb9d39b25e6b
incubator4 <aries0robin@gmail.com>
2023-12-05 22:22:19 +0800

    feat(consul): delete unused consul

--- application_set.tf
+++ application_set.tf
@@ -360,62 +360,62 @@ resource "argocd_application_set" "cert_manager" {
   }
 }
 
-resource "argocd_application_set" "consul" {
-  metadata {
-    name = "consul"
-  }
-  spec {
-    generator {
-      list {
-        elements = [
-          {
-            cluster = argocd_cluster.dev.name
-            url     = argocd_cluster.dev.server
-          },
-          #          {
-          #            cluster = argocd_cluster.prod.name
-          #            url     = argocd_cluster.prod.server
-          #          }
-        ]
-      }
-    }
-    template {
-      metadata {
-        name = "consul-{{cluster}}"
-        labels = {
-          cluster = "{{cluster}}"
-        }
-      }
-
-      spec {
-        project = argocd_project.guardian.metadata[0].name
-
-        source {
-          repo_url        = var.repo_url
-          target_revision = "HEAD"
-          path            = "consul/{{cluster}}"
-          plugin {
-            name = "avp-kustomize"
-            env {
-              name = "APP_REPO"
-              value = "NaturalSelectionLabs/Hephaestus"
-            }
-            env {
-              name = "AVP_SECRET"
-              value = "guardian:avp-{{cluster}}"
-            }
-          }
-        }
-
-        destination {
-          server    = "{{url}}"
-          namespace = "guardian"
-        }
-
-      }
-    }
-  }
-}
+#resource "argocd_application_set" "consul" {
+#  metadata {
+#    name = "consul"
+#  }
+#  spec {
+#    generator {
+#      list {
+#        elements = [
+#          {
+#            cluster = argocd_cluster.dev.name
+#            url     = argocd_cluster.dev.server
+#          },
+#          #          {
+#          #            cluster = argocd_cluster.prod.name
+#          #            url     = argocd_cluster.prod.server
+#          #          }
+#        ]
+#      }
+#    }
+#    template {
+#      metadata {
+#        name = "consul-{{cluster}}"
+#        labels = {
+#          cluster = "{{cluster}}"
+#        }
+#      }
+#
+#      spec {
+#        project = argocd_project.guardian.metadata[0].name
+#
+#        source {
+#          repo_url        = var.repo_url
+#          target_revision = "HEAD"
+#          path            = "consul/{{cluster}}"
+#          plugin {
+#            name = "avp-kustomize"
+#            env {
+#              name = "APP_REPO"
+#              value = "NaturalSelectionLabs/Hephaestus"
+#            }
+#            env {
+#              name = "AVP_SECRET"
+#              value = "guardian:avp-{{cluster}}"
+#            }
+#          }
+#        }
+#
+#        destination {
+#          server    = "{{url}}"
+#          namespace = "guardian"
+#        }
+#
+#      }
+#    }
+#  }
+#}
 
 resource "argocd_application_set" "crdb" {
   metadata {
