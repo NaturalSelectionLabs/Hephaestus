7c34c083b90d65ebe5409a7f5bacf3df02e1ff55
incubator4 <aries0robin@gmail.com>
2023-12-31 18:41:19 +0800

    feat: grafana

--- application.tf
+++ application.tf
@@ -1,33 +1,33 @@
-#resource "argocd_application" "grafana" {
-#  metadata {
-#    name      = "grafana"
-#    namespace = "guardian"
-#  }
-#  spec {
-#    project = argocd_project.guardian.metadata[0].name
-#    source {
-#      repo_url        = var.repo_url
-#      target_revision = "HEAD"
-#      path            = "grafana/prod"
-#      plugin {
-#        name = "avp-kustomize"
-#        env {
-#          name = "APP_REPO"
-#          value = "NaturalSelectionLabs/Hephaestus"
-#        }
-#        env {
-#          name = "AVP_SECRET"
-#          value = "guardian:avp-prod"
-#        }
-#      }
-#    }
-#
-#    destination {
-#      server    = argocd_cluster.prod.server
-#      namespace = "guardian"
-#    }
-#  }
-#}
+resource "argocd_application" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = "guardian"
+  }
+  spec {
+    project = argocd_project.guardian.metadata[0].name
+    source {
+      repo_url        = var.repo_url
+      target_revision = "HEAD"
+      path            = "grafana/prod"
+      plugin {
+        name = "avp-kustomize"
+        env {
+          name = "APP_REPO"
+          value = "NaturalSelectionLabs/Hephaestus"
+        }
+        env {
+          name = "AVP_SECRET"
+          value = "guardian:avp-prod"
+        }
+      }
+    }
+
+    destination {
+      server    = argocd_cluster.prod.server
+      namespace = "guardian"
+    }
+  }
+}
 
 resource "argocd_application" "keycloak" {
   metadata {
