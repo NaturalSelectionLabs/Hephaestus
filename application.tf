0ffe49c563c14544750060df6ed8bc40828abcbd
incubator4 <aries0robin@gmail.com>
2024-01-08 20:14:47 +0800

    feat: ipfs

--- application.tf
+++ application.tf
@@ -186,3 +186,35 @@ resource "argocd_application" "discourse" {
     }
   }
 }
+
+resource "argocd_application" "ipfs" {
+  metadata {
+    name      = "ipfs"
+    namespace = "guardian"
+  }
+  spec {
+    project = argocd_project.guardian.metadata[0].name
+
+    source {
+      repo_url        = var.repo_url
+      target_revision = "HEAD"
+      path            = "ipfs/prod"
+      plugin {
+        name = "avp-kustomize"
+        env {
+          name = "APP_REPO"
+          value = "NaturalSelectionLabs/Hephaestus"
+        }
+        env {
+          name = "AVP_SECRET"
+          value = "guardian:avp-prod"
+        }
+      }
+    }
+
+    destination {
+      server    = argocd_cluster.prod.server
+      namespace = "guardian"
+    }
+  }
+}
