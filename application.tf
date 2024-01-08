b75f60e37e58fdf0405b910b33f2d59784c69309
incubator4 <aries0robin@gmail.com>
2024-01-08 16:33:07 +0800

    chore(jaeger): dev

--- application.tf
+++ application.tf
@@ -124,38 +124,6 @@ resource "argocd_application" "rabbitmq" {
   }
 }
 
-resource "argocd_application" "jaeger" {
-  metadata {
-    name      = "jaeger"
-    namespace = "guardian"
-  }
-  spec {
-    project = argocd_project.guardian.metadata[0].name
-
-    source {
-      repo_url        = var.repo_url
-      target_revision = "HEAD"
-      path            = "jaeger/prod"
-      plugin {
-        name = "avp-kustomize"
-        env {
-          name  = "APP_REPO"
-          value = "NaturalSelectionLabs/Hephaestus"
-        }
-        env {
-          name  = "AVP_SECRET"
-          value = "guardian:avp-prod"
-        }
-      }
-    }
-
-    destination {
-      server    = argocd_cluster.prod.server
-      namespace = "guardian"
-    }
-  }
-}
-
 resource "argocd_application" "discourse" {
   metadata {
     name      = "discourse"
