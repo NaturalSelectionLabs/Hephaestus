5ad39fad1a645fdd01a9ad3b1e405d427eb94b51
incubator4 <aries0robin@gmail.com>
2024-01-30 20:56:44 +0800

    chore(clickhouse): remove

--- application_set.tf
+++ application_set.tf
@@ -651,60 +651,60 @@ resource "argocd_application_set" "jaeger" {
 
   }
 }
-
-resource "argocd_application_set" "clickhouse" {
-  metadata {
-    name      = "clickhouse"
-    namespace = "guardian"
-  }
-  spec {
-    generator {
-      list {
-        elements = [
-          {
-            cluster = argocd_cluster.dev.name
-            url     = argocd_cluster.dev.server
-          },
-          {
-            cluster = argocd_cluster.prod.name
-            url     = argocd_cluster.prod.server
-          }
-        ]
-      }
-    }
-    template {
-      metadata {
-        name = "clickhouse-{{cluster}}"
-        labels = {
-          cluster = "{{cluster}}"
-        }
-      }
-      spec {
-        project = argocd_project.guardian.metadata[0].name
-
-        source {
-          repo_url        = var.repo_url
-          target_revision = "HEAD"
-          path            = "clickhouse/{{cluster}}"
-          plugin {
-            name = "avp-kustomize"
-            env {
-              name  = "APP_REPO"
-              value = "NaturalSelectionLabs/Hephaestus"
-            }
-            env {
-              name  = "AVP_SECRET"
-              value = "guardian:avp-{{cluster}}"
-            }
-          }
-        }
-
-        destination {
-          server    = "{{url}}"
-          namespace = "guardian"
-        }
-      }
-    }
-
-  }
-}
+#
+#resource "argocd_application_set" "clickhouse" {
+#  metadata {
+#    name      = "clickhouse"
+#    namespace = "guardian"
+#  }
+#  spec {
+#    generator {
+#      list {
+#        elements = [
+#          {
+#            cluster = argocd_cluster.dev.name
+#            url     = argocd_cluster.dev.server
+#          },
+#          {
+#            cluster = argocd_cluster.prod.name
+#            url     = argocd_cluster.prod.server
+#          }
+#        ]
+#      }
+#    }
+#    template {
+#      metadata {
+#        name = "clickhouse-{{cluster}}"
+#        labels = {
+#          cluster = "{{cluster}}"
+#        }
+#      }
+#      spec {
+#        project = argocd_project.guardian.metadata[0].name
+#
+#        source {
+#          repo_url        = var.repo_url
+#          target_revision = "HEAD"
+#          path            = "clickhouse/{{cluster}}"
+#          plugin {
+#            name = "avp-kustomize"
+#            env {
+#              name  = "APP_REPO"
+#              value = "NaturalSelectionLabs/Hephaestus"
+#            }
+#            env {
+#              name  = "AVP_SECRET"
+#              value = "guardian:avp-{{cluster}}"
+#            }
+#          }
+#        }
+#
+#        destination {
+#          server    = "{{url}}"
+#          namespace = "guardian"
+#        }
+#      }
+#    }
+#
+#  }
+#}
