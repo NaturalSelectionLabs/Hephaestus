de9755cd76451b434a1068da1b5aaf915c50eddc
incubator4 <aries0robin@gmail.com>
2024-02-07 12:15:36 +0800

    chore(exporter): add dev

--- application_set.tf
+++ application_set.tf
@@ -661,10 +661,10 @@ resource "argocd_application_set" "exporter" {
     generator {
       list {
         elements = [
-#          {
-#            cluster = argocd_cluster.dev.name
-#            url     = argocd_cluster.dev.server
-#          },
+          {
+            cluster = argocd_cluster.dev.name
+            url     = argocd_cluster.dev.server
+          },
           {
             cluster = argocd_cluster.prod.name
             url     = argocd_cluster.prod.server
