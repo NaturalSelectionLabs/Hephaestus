10ead4abb56786d9d603afbd91188721db2ba98b
incubator4 <aries0robin@gmail.com>
2024-02-20 14:14:14 +0800

    chore(rabbitmq): add dev

--- application_set.tf
+++ application_set.tf
@@ -765,3 +765,59 @@ resource "argocd_application_set" "alert" {
 
   }
 }
+
+resource "argocd_application_set" "rabbitmq" {
+  metadata {
+    name      = "rabbitmq"
+    namespace = "guardian"
+  }
+  spec {
+    generator {
+      list {
+        elements = [
+          {
+            cluster = argocd_cluster.dev.name
+            url     = argocd_cluster.dev.server
+          },
+          {
+            cluster = argocd_cluster.prod.name
+            url     = argocd_cluster.prod.server
+          }
+        ]
+      }
+    }
+    template {
+      metadata {
+        name = "rabbitmq-{{cluster}}"
+        labels = {
+          cluster = "{{cluster}}"
+        }
+      }
+      spec {
+        project = argocd_project.guardian.metadata[0].name
+
+        source {
+          repo_url        = var.repo_url
+          target_revision = "HEAD"
+          path            = "rabbitmq/{{cluster}}"
+          plugin {
+            name = "avp-kustomize"
+            env {
+              name  = "APP_REPO"
+              value = "NaturalSelectionLabs/Hephaestus"
+            }
+            env {
+              name  = "AVP_SECRET"
+              value = "guardian:avp-prod"
+            }
+          }
+        }
+
+        destination {
+          server    = "{{url}}"
+          namespace = "guardian"
+        }
+      }
+    }
+  }
+}
