126e8c75a1aa0c4a992195a3f283e2b7a11eb08e
incubator4 <aries0robin@gmail.com>
2024-02-23 14:29:39 +0800

    chore: add crossbell crdb

--- application_set.tf
+++ application_set.tf
@@ -474,6 +474,63 @@ resource "argocd_application_set" "crdb" {
   }
 }
 
+resource "argocd_application_set" "crdb-csb" {
+  metadata {
+    name = "crdb-csb"
+  }
+  spec {
+    generator {
+      list {
+        elements = [
+#          {
+#            cluster = argocd_cluster.dev.name
+#            url     = argocd_cluster.dev.server
+#          },
+          {
+            cluster = argocd_cluster.prod.name
+            url     = argocd_cluster.prod.server
+          }
+        ]
+      }
+    }
+    template {
+      metadata {
+        name = "crdb-csb-{{cluster}}"
+        labels = {
+          cluster = "{{cluster}}"
+        }
+      }
+
+      spec {
+        project = argocd_project.guardian.metadata[0].name
+
+        source {
+          repo_url        = var.repo_url
+          target_revision = "HEAD"
+          path            = "crdb/crossbell/{{cluster}}"
+          plugin {
+            name = "avp-kustomize"
+            env {
+              name  = "APP_REPO"
+              value = "NaturalSelectionLabs/Hephaestus"
+            }
+            env {
+              name  = "AVP_SECRET"
+              value = "guardian:avp-{{cluster}}"
+            }
+          }
+        }
+
+        destination {
+          server    = "{{url}}"
+          namespace = "crossbell"
+        }
+
+      }
+    }
+  }
+}
+
 resource "argocd_application_set" "kafka" {
   metadata {
     name = "kafka"
