c9223c56c5da1954ffea255e45162c4f6e63cd8a
incubator4 <aries0robin@gmail.com>
2024-04-12 15:46:29 +0800

    chore: add keda

--- application_set.tf
+++ application_set.tf
@@ -869,3 +869,57 @@ resource "argocd_application_set" "etcd" {
     }
   }
 }
+
+
+resource "argocd_application_set" "keda" {
+  metadata {
+    name = "keda"
+  }
+  spec {
+    generator {
+      list {
+        elements = [
+#          {
+#            cluster = argocd_cluster.dev.name
+#            url     = argocd_cluster.dev.server
+#          },
+          {
+            cluster = argocd_cluster.prod.name
+            url     = argocd_cluster.prod.server
+          }
+        ]
+      }
+    }
+    template {
+      metadata {
+        name = "keda-{{cluster}}"
+      }
+
+      spec {
+        project = argocd_project.guardian.metadata[0].name
+
+        source {
+          repo_url        = var.repo_url
+          target_revision = "HEAD"
+          path            = "keda/{{cluster}}"
+          plugin {
+            name = "avp-kustomize"
+            env {
+              name  = "APP_REPO"
+              value = "NaturalSelectionLabs/Hephaestus"
+            }
+            env {
+              name  = "AVP_SECRET"
+              value = "guardian:avp-{{cluster}}"
+            }
+          }
+        }
+
+        destination {
+          server    = "{{url}}"
+          namespace = "guardian"
+        }
+      }
+    }
+  }
+}
