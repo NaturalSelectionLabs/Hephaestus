f9808e3562bc97ff40fac25b40f4887e486cb12b
incubator4 <aries0robin@gmail.com>
2024-06-27 16:42:03 +0800

    chore(argo-workflow): add prod

--- application_set.tf
+++ application_set.tf
@@ -927,11 +927,11 @@ resource "argocd_application_set" "argo-workflow" {
           {
             cluster = argocd_cluster.dev.name
             url     = argocd_cluster.dev.server
+          },
+          {
+            cluster = argocd_cluster.prod.name
+            url     = argocd_cluster.prod.server
           }
-          #           {
-          #             cluster = argocd_cluster.prod.name
-          #             url     = argocd_cluster.prod.server
-          #           }
         ]
       }
     }
