c058a86f054cda632237a88ae52ea5c075e13084
incubator4 <aries0robin@gmail.com>
2024-06-27 15:31:30 +0800

    feat: add argo-workflow

--- application_set.tf
+++ application_set.tf
@@ -915,3 +915,56 @@ resource "argocd_application_set" "thumbor" {
     }
   }
 }
+
+resource "argocd_application_set" "argo-workflow" {
+  metadata {
+    name = "argo-workflow"
+  }
+  spec {
+    generator {
+      list {
+        elements = [
+          {
+            cluster = argocd_cluster.dev.name
+            url     = argocd_cluster.dev.server
+          }
+          #           {
+          #             cluster = argocd_cluster.prod.name
+          #             url     = argocd_cluster.prod.server
+          #           }
+        ]
+      }
+    }
+    template {
+      metadata {
+        name = "argo-workflow-{{cluster}}"
+      }
+
+      spec {
+        project = argocd_project.guardian.metadata[0].name
+
+        source {
+          repo_url        = var.repo_url
+          target_revision = "HEAD"
+          path            = "argo-workflow/{{cluster}}"
+          plugin {
+            name = "avp-kustomize"
+            env {
+              name  = "APP_REPO"
+              value = "NaturalSelectionLabs/Hephaestus"
+            }
+            env {
+              name  = "AVP_SECRET"
+              value = "guardian:avp-{{cluster}}"
+            }
+          }
+        }
+
+        destination {
+          server    = "{{url}}"
+          namespace = "guardian"
+        }
+      }
+    }
+  }
+}
