5c63678394bfb513ca1f78b15adad2d2ce186e9f
incubator4 <aries0robin@gmail.com>
2024-12-04 15:09:39 +0800

    feat: add cloudnative pg

--- application_set.tf
+++ application_set.tf
@@ -758,3 +758,51 @@ resource "argocd_application_set" "cloud-sql-proxy" {
     }
   }
 }
+
+resource "argocd_application_set" "cloud_native_pg" {
+  metadata {
+    name = "cloud-native-pg"
+  }
+  spec {
+    go_template = true
+    generator {
+      clusters {
+        selector {
+          match_labels = {
+            "argocd.argoproj.io/secret-type" = "cluster"
+            "cluster-type" = "gke-standard"
+          }
+        }
+      }
+    }
+    template {
+      metadata {
+        name = "cloud-native-pg-{{.name}}"
+        labels = {
+          cluster = "{{.name}}"
+          env     = "{{.metadata.labels.env}}"
+        }
+      }
+
+      spec {
+        project = argocd_project.guardian.metadata[0].name
+
+        source {
+          repo_url        = var.repo_url
+          target_revision = "HEAD"
+          path            = "cloud-native-pg"
+          kustomize {
+            common_annotations = {
+              "github.com/url" = var.repo_url
+            }
+          }
+        }
+
+        destination {
+          name      = "{{.name}}"
+          namespace = "guardian"
+        }
+      }
+    }
+  }
+}
