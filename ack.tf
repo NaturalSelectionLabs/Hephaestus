dd57198fe0ca136a425f1ff4edede5dac6ba625f
incubator4 <aries0robin@gmail.com>
2025-09-01 15:19:31 +0800

    refactor(terraform): consolidate Kubernetes cluster data sources into a local variable for improved management

--- ack.tf
+++ ack.tf
@@ -1,7 +1,14 @@
-data "alicloud_cs_kubernetes_clusters" "common" {
-  ids = "c0f3a81dd784c40b1bd75f140332da998"
+locals {
+  cluster_ids = {
+    common = "c0f3a81dd784c40b1bd75f140332da998"
+    folo   = "cb75cfa28dac844d6ac175e587ee60801"
+  }
 }
 
-data "alicloud_cs_kubernetes_clusters" "folo" {
-  ids = "cb75cfa28dac844d6ac175e587ee60801"
+data "alicloud_cs_kubernetes_clusters" "cluster" {
+  ids = [for k, v in local.cluster_ids : v]
+}
+
+output "clusters" {
+  value = data.alicloud_cs_kubernetes_clusters.cluster.clusters
 }
