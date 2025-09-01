36b394d4963d0ff7d1842c4984edb4d323a8fef9
incubator4 <aries0robin@gmail.com>
2025-09-01 17:21:50 +0800

    fix(terraform): update Argocd cluster configuration to use the correct certificate authority reference for TLS client

--- cluster.tf
+++ cluster.tf
@@ -122,7 +122,7 @@ resource "argocd_cluster" "folo" {
   config {
     bearer_token = module.folo-argocd-manager.bearer_token
     tls_client_config {
-      ca_data = base64decode(data.alicloud_cs_cluster_credential.folo.kube_config.cluster_ca_certificate)
+      ca_data = base64decode(data.alicloud_cs_cluster_credential.folo.certificate_authority.cluster_cert)
     }
   }
 }
