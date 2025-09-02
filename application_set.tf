e856dfc64dd8c703473f5391dba00c9934644daf
incubator4 <aries0robin@gmail.com>
2025-09-02 10:59:46 +0800

    feat(terraform): add match_expressions to Argocd application set for provider filtering

--- application_set.tf
+++ application_set.tf
@@ -62,6 +62,12 @@ resource "argocd_application_set" "victoria_metrics" {
           match_labels = {
             "argocd.argoproj.io/secret-type" = "cluster"
           }
+
+          match_expressions {
+            key      = "provider"
+            operator = "NotIn"
+            values   = ["alicloud"]
+          }
         }
       }
     }
